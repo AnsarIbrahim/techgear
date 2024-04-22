aeb54971d348308b9b5a31a1cd6135c3c2cdbbc9
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2024-04-23 00:30:56 +0530

    Add Basic Setup and install Bootstrap

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import PageRoute from "./Route/PageRoute";
+
+const App = () => {
+  return (
+    <div className="text-center">
+      <PageRoute />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      Home
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
